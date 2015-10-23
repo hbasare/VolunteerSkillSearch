f98eb64c870772870de4310e539a7d3a0ce2ef5e
harry-haller <harry-haller@users.noreply.github.com>
2015-10-23 18:53:07 -0300

    updated admin table with the requirements in the business plan

--- volunteerapp/src/main/java/volunteerapp/voludb/CreateDB.java
+++ volunteerapp/src/main/java/volunteerapp/voludb/CreateDB.java
@@ -12,10 +12,17 @@ public class CreateDB {
 	public void createAdminUsers() throws ClassNotFoundException, SQLException   {
 		Class.forName(DRIVER);
 		Connection connection=DriverManager.getConnection(JDBC_URL);
-		connection.createStatement().execute("create table adminusers(username varchar(50), password varchar(50))");
+		connection.createStatement().execute("drop table adminusers");
+		connection.createStatement().execute("create table adminusers(firstname varchar(50), "
+				+ "lastname varchar(50), "
+				+ "companyname varchar(50), "
+				+ "phonenumber varchar(50), "
+				+ "email varchar(50), "
+				+ "username varchar(50), "
+				+ "password varchar(50))");
 		connection.createStatement().execute("insert into adminusers values "+
-										     "('admin1','1234'),"+
-											 "('admin2','1234')");
+										     "('john','doe','company1','555-555-5555','admin1@company1.com','admin1','1234'),"+
+											 "('jane','doe','company2','555-666-6666','admin2@company2.com','admin2','1234')");
 		System.out.println("adminusers table created and records sucessfully inserted.");
 
 		
